5468bde20b098b0d37d90c365879bcf1d32b1df6
SakharovaKsu <nagis38@gmail.com>
2023-07-02 17:53:02 +0300

    Начала прописывать логику для фильтрации

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import {v1} from 'uuid';
 import {ShoppingList} from './components/ShoppingList';
 
 
-export type ProductType = 'Product' | 'Bakery' | 'Milk' | 'Meat'
+export type ProductType = 'Product' | 'Bakery' | 'Milk' | 'Meat' | 'Sweets'
 export type СheckedType = 'All' | 'Active' | 'Completed'
+
+export type FilterProduct = ProductType | СheckedType
 
 export type ShoppingListType = {
   id: string
@@ -20,12 +22,15 @@ const ShoppingListData: ShoppingListType[] = [
   {id: v1(), title: 'Chicken', product: 'Meat', checked: true},
   {id: v1(), title: 'Svineniya', product: 'Meat', checked: false},
   {id: v1(), title: 'Buns', product: 'Bakery', checked: false},
+  {id: v1(), title: 'Candy', product: 'Sweets', checked: false}
 ]
 
 export default function App() {
 
   const [shopping, setShopping] = useState<ShoppingListType[]>(ShoppingListData)
   const [value, setValue] = useState<string>('')
+  const [filterProduct, setFilterProduct] = useState<ProductType>('Product')
+  const [filterStatus, setFilterStatus] = useState<СheckedType>('All')
 
   // Добавление продукта
   const addProduct = (product: ProductType) => {
@@ -37,14 +42,54 @@ export default function App() {
   const removeProduct = (id: string) => {
     setShopping(shopping.filter(s => s.id !== id))
   }
+
+  const getFilterProductStatus = (product: ShoppingListType[], filter: СheckedType) => {
+    switch (filter) {
+      case 'Active':
+        return shopping.filter(s => !s.checked)
+      case 'Completed':
+        return shopping.filter(s => s.checked)
+      default:
+        return shopping
+    }
+  }
+
+  const getFilterProduct = (product: ShoppingListType[], filter: ProductType) => {
+    switch (filter) {
+      case 'Bakery':
+        return shopping.filter(s => s.product === 'Bakery')
+      case 'Milk':
+        return shopping.filter(s => s.product === 'Milk')
+      case 'Meat':
+        return shopping.filter(s => s.product === 'Meat')
+      case 'Sweets':
+        return shopping.filter(s => s.product === 'Sweets')
+      default:
+        return shopping
+    }
+  }
+
+  const filteredStatusProduct = getFilterProductStatus(shopping, filterStatus)
+  const filteredProduct = getFilterProduct(shopping, filterProduct)
+
+  const changeFilterStatus = (filterStatus:СheckedType) => {
+    setFilterStatus(filterStatus)
+  }
+
+  const changeFilterProduct= (filterProduct:ProductType) => {
+    setFilterProduct(filterProduct)
+  }
+
   return (
     <div className="App">
       <h1>Sopping list</h1>
-      <ShoppingList shopping={shopping}
+      <ShoppingList shopping={filteredProduct}
                     value={value}
                     setValue={setValue}
                     addProduct={addProduct}
                     removeProduct={removeProduct}
+                    changeFilterStatus={changeFilterStatus}
+                    changeFilterProduct={changeFilterProduct}
       />
     </div>
   );
--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,5 +1,5 @@
-import React, {ChangeEvent, ChangeEventHandler, FC, MouseEventHandler, useState} from 'react';
-import {ProductType, ShoppingListType} from '../App';
+import React, {ChangeEvent, FC, FormEvent, KeyboardEvent, useState} from 'react';
+import {FilterProduct, ProductType, ShoppingListType, СheckedType} from '../App';
 import s from './ShoppingList.module.css'
 
 type ShoppingListPropsType = {
@@ -8,6 +8,8 @@ type ShoppingListPropsType = {
     setValue: (text:string) => void
     addProduct: (product: ProductType) => void
     removeProduct: (id: string) => void
+    changeFilterStatus: (filter: СheckedType) => void
+    changeFilterProduct: (filter: ProductType) => void
 }
 
 export const ShoppingList:FC<ShoppingListPropsType> = (
@@ -16,11 +18,14 @@ export const ShoppingList:FC<ShoppingListPropsType> = (
         value,
         setValue,
         addProduct,
-        removeProduct
+        removeProduct,
+        changeFilterStatus,
+        changeFilterProduct
     }) => {
 
     const [product, setProduct] = useState<ProductType>('Product')
 
+    // Добавление продукта
     const AddButtonHandler = () => {
         if(product !== 'Product') {
            if(value.trim() !== '') {
@@ -32,16 +37,38 @@ export const ShoppingList:FC<ShoppingListPropsType> = (
 
     }
 
+    // добавление по Enter
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter') {
+            AddButtonHandler()
+        }
+    }
+
+    // удаление продукта
     const RemoveButtonHandler = (id: string) => {
         removeProduct(id)
     }
 
+    // Выбираем то, что есть в select при добавлении продукта
     const onChangeProductHandler = (e: ChangeEvent<HTMLSelectElement>) => {
         setProduct(e.currentTarget.value as ProductType)
     }
 
+    // Получаем из инпута value
     const onChangeValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.currentTarget.value)
+    }
+
+    // Узнаем значение статуса у select
+    const onChangeStatusHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        const filter = e.currentTarget.value as СheckedType
+        changeFilterStatus(filter)
+    }
+
+    // Узнаем какой тип продукта в select
+    const onChangeFilterProduct = (e: ChangeEvent<HTMLSelectElement>) => {
+        const filter = e.currentTarget.value as ProductType
+        changeFilterProduct(filter)
     }
 
     const list = shopping.map(l => {
@@ -50,7 +77,7 @@ export const ShoppingList:FC<ShoppingListPropsType> = (
                 <input type="checkbox" checked={l.checked}/>
                 <span> {l.title} </span>
                 <span> Product: {l.product}</span>
-                <button className={s.button+ ' ' + s.buttonSmall} onClick={() => RemoveButtonHandler(l.id)}>X</button>
+                <button className={s.button+ ' ' + s.buttonSmall} onClick={() => RemoveButtonHandler(l.id)}>—</button>
             </li>
         )
     })
@@ -58,28 +85,24 @@ export const ShoppingList:FC<ShoppingListPropsType> = (
     return (
         <div className={s.box}>
             <h2>Purchases for today</h2>
-            <div>
-                <input placeholder={'What to buy'}
-                       value={value}
-                       onChange={onChangeValueHandler}/>
-                <select value={product} onChange={onChangeProductHandler}>
-                    <option value={'Product'}>Product</option>
-                    <option value={'Bakery'}>Bakery</option>
-                    <option value={'Milk'}>Milk</option>
-                    <option value={'Meat'}>Meat</option>
-                    <option value={'Sweets'}>Sweets</option>
-                </select>
-                <button className={s.button + ' ' + s.buttonBig} onClick={AddButtonHandler}>Add</button>
-            </div>
-            <div>
-                <ul className={s.list}>
-                    {list}
-                </ul>
-            </div>
             <div className={s.container}>
                 <div>
+                    <input placeholder={'What to buy'}
+                           value={value}
+                           onChange={onChangeValueHandler}
+                           onKeyDown={onKeyDownHandler}/>
+                    <select value={product} onChange={onChangeProductHandler}>
+                        <option value={'Product'}>Product</option>
+                        <option value={'Bakery'}>Bakery</option>
+                        <option value={'Milk'}>Milk</option>
+                        <option value={'Meat'}>Meat</option>
+                        <option value={'Sweets'}>Sweets</option>
+                    </select>
+                    <button className={s.button + ' ' + s.buttonBig} onClick={AddButtonHandler}>Add</button>
+                </div>
+                <div>
                     <h3 className={s.subTitle}>Filter by active</h3>
-                    <select>
+                    <select onChange={onChangeStatusHandler}>
                         <option value={'All'}>All</option>
                         <option value={'Active'}>Active</option>
                         <option value={'Completed'}>Completed</option>
@@ -87,7 +110,8 @@ export const ShoppingList:FC<ShoppingListPropsType> = (
                 </div>
                 <div>
                     <h3 className={s.subTitle}>Filter by product</h3>
-                    <select>
+                    <select onChange={onChangeFilterProduct}>
+                        <option value={'Product'}>Product</option>
                         <option value={'Bakery'}>Bakery</option>
                         <option value={'Milk'}>Milk</option>
                         <option value={'Meat'}>Meat</option>
@@ -95,6 +119,13 @@ export const ShoppingList:FC<ShoppingListPropsType> = (
                     </select>
                 </div>
             </div>
+
+            <div>
+                <ul className={s.list}>
+                    {list}
+                </ul>
+            </div>
+
         </div>
     );
 };
